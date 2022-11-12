028abb8c388e7f71314b2b7bc976c8639890281e
million333 <107103763+million333@users.noreply.github.com>
2022-11-12 00:02:53 -0800

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,6 +1,4 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
-export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
 export PATH_shadowsocks=${PATH_shadowsocks-/shadowsocks/$UUID}
